a9d251bcb9ec99b5696a8582d45533d39f0858dd
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-03-13 16:12:29 +0800

    relaese 0.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 Package.describe({
   name: 'mubiale:web3',
-  version: '0.15.3',
+  version: '0.0.1',
   summary: 'Ethereum JavaScript API, middleware to talk to a ethreum node over RPC',
   git: 'https://github.com/ethereum/ethereum.js',
   // By default, Meteor will default to using README.md for documentation.
